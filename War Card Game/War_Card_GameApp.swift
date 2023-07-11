b21359fa0c82f58b6d207ecc3c1fb0b313d70a6b
Sams-E-Tabrige <Sams.tabrige@mykenzie.snhu.edu>
2023-07-11 00:04:19 -0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sams Tabrige on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
